57bcc804a886a2248d4e1a1a8d3edab0dbc79a08
Phong <phong.bach@wsu.edu>
2020-11-12 20:21:20 -0800

    Add complete implementation for story optimization

--- src/main.py
+++ src/main.py
@@ -7,62 +7,40 @@ from tensorflow.python import platform
 from unidecode import unidecode
 import string
 
-
-def pre_process(corpus):
-    # convert input corpus to lower case.
-    corpus = corpus.lower()
-    # collecting a list of stop words from nltk and punctuation form
-    # string class and create single array.
-    stopset = stopwords.words('english') + list(string.punctuation)
-    # remove stop words and punctuations from string.
-    # word_tokenize is used to tokenize the input corpus in word tokens.
-    corpus = " ".join([i for i in word_tokenize(corpus) if i not in stopset])
-    # remove non-ascii characters
-    corpus = unidecode(corpus)
-    return corpus
-
-
-print(pre_process("Sample of non ASCII: Ceñía. How to remove stopwords and punctuations?"))
-
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-
-lemmatizer = WordNetLemmatizer()
-sentence = "The striped bats are hanging on their feet for best"
-words = word_tokenize(sentence)
-
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-# sentence pair
-corpus = ["A girl is styling her hair.", "A girl is brushing her hair."]
-for c in range(len(corpus)):
-    corpus[c] = pre_process(corpus[c])
-# creating vocabulary using uni-gram and bi-gram
-tfidf_vectorizer = TfidfVectorizer(ngram_range=(1, 2))
-tfidf_vectorizer.fit(corpus)
-
-feature_vectors = tfidf_vectorizer.transform(corpus)
+# def pre_process(corpus):
+#     # convert input corpus to lower case.
+#     corpus = corpus.lower()
+#     # collecting a list of stop words from nltk and punctuation form
+#     # string class and create single array.
+#     stopset = stopwords.words('english') + list(string.punctuation)
+#     # remove stop words and punctuations from string.
+#     # word_tokenize is used to tokenize the input corpus in word tokens.
+#     corpus = " ".join([i for i in word_tokenize(corpus) if i not in stopset])
+#     # remove non-ascii characters
+#     corpus = unidecode(corpus)
+#     return corpus
 
 from gensim.models import Word2Vec
 import gensim as gensim
 import numpy as np
+from collections import Counter
+import itertools
 
 # Load Google's pre-trained Word2Vec model.
 word_emb_model = gensim.models.KeyedVectors.load_word2vec_format('./GoogleNews-vectors-negative300.bin', binary=True)
 
-from collections import Counter
-import itertools
 
-
+# Map words frequency in document
 def map_word_frequency(document):
     return Counter(itertools.chain(*document))
 
 
+# Return the sif feature vectors for two sentences
 def get_sif_feature_vectors(sentence1, sentence2, word_emb_model=word_emb_model):
     sentence1 = [token for token in sentence1.split() if token in word_emb_model.vocab]
     sentence2 = [token for token in sentence2.split() if token in word_emb_model.vocab]
     word_counts = map_word_frequency((sentence1 + sentence2))
-    embedding_size = 300  # size of vectore in word embeddings
+    embedding_size = 300  # size of vector in word embeddings
     a = 0.001
     sentence_set = []
     for sentence in [sentence1, sentence2]:
@@ -79,45 +57,26 @@ def get_sif_feature_vectors(sentence1, sentence2, word_emb_model=word_emb_model)
 from sklearn.metrics.pairwise import cosine_similarity
 
 
+# Return the cosine similarity between two vectors
 def get_cosine_similarity(feature_vec_1, feature_vec_2):
     return cosine_similarity(feature_vec_1.reshape(1, -1), feature_vec_2.reshape(1, -1))[0][0]
 
 
-# print("Comparison")
-#
-# vector1 = "The spreadsheet should have Columns A to Z and Rows 1 to 50."
-#
-# vector2 = "The cells should be referencable from their corresponding name (Example: A1, B42, etc.)."
-#
-# vector3 = "Cells can reference other cells for expressions or text."
-#
-# vector4 = "Update cells that are referencing other cells when the referenced cell is updated."
-#
-# print("Get feature vector 1 and 2")
-# feature_vector12 = get_sif_feature_vectors(vector1, vector2, word_emb_model)
-# print("Get comparison between 1 and 2")
-# print(get_cosine_similarity(feature_vector12[0], feature_vector12[1]))
-#
-# print("Get feature vector 1 and 3")
-# feature_vector13 = get_sif_feature_vectors(vector1, vector3, word_emb_model)
-# print("Get comparison between 1 and 3")
-# print(get_cosine_similarity(feature_vector13[0], feature_vector13[1]))
-#
-# print("Get feature vector 1 and 4")
-# feature_vector14 = get_sif_feature_vectors(vector1, vector4, word_emb_model)
-# print("Get comparison between 1 and 4")
-# print(get_cosine_similarity(feature_vector14[0], feature_vector14[1]))
-
 # Split paragraph into an array and eliminate bad sentence
-def pre_process_story(story):
-    story_sentences = story.split(".")
+def pre_process_story(story_input):
+    story_sentences = story_input.split(".")
 
     # Remove bad sentences
     for story_sentence in story_sentences:
         if len(story_sentence) < 5:
             story_sentences.remove(story_sentence)
 
+    # Strip white spaces
+    for i in range(len(story_sentences)):
+        story_sentences[i] = story_sentences[i].strip()
+
     return story_sentences
+
 
 # Function decision to generate comparison coefficient
 def function_decision(story_sentences):
@@ -126,56 +85,111 @@ def function_decision(story_sentences):
     for i in range(0, len(story_sentences)):
         for j in range(i + 1, len(story_sentences)):
             feature_vector = get_sif_feature_vectors(story_sentences[i], story_sentences[j], word_emb_model)
-            cosine_similarity = get_cosine_similarity(feature_vector[0], feature_vector[1])
-            my_dict[cosine_similarity] = (story_sentences[i], story_sentences[j])
+            cosine_similarity_value = get_cosine_similarity(feature_vector[0], feature_vector[1])
+            my_dict[cosine_similarity_value] = (story_sentences[i], story_sentences[j])
     return my_dict
+
 
 story = "The spreadsheet should have Columns A to Z and Rows 1 to 50. The cells should be referencable from their " \
         "corresponding name (Example: A1, B42, etc.). Cells can reference other cells for expressions or text." \
         "Update cells that are referencing other cells when the referenced cell is updated. "
 
-print(pre_process_story(story))
-print(len(pre_process_story(story)))
+story2 = "The spreadsheet should have Columns A to Z and Rows 1 to 50. The cells should be referenceable from their " \
+         "corresponding name (Example: A1, B42, etc.). Cells can reference other cells for expressions or text. Update " \
+         "cells that are referencing other cells when the referenced cell is updated. Cells should be able to evaluate " \
+         "mathematical statements. Arithmetic expressions are represented as trees. Support for addition, subtraction, " \
+         "multiplication, division, and parentheses. Add a background color to cells that can be any RGB color. Be " \
+         "able to change the background color of many cells at once. Allow for color and text changing to be undone. " \
+         "Be able to redo any undone changes. Allow for the contents of the spreadsheet to be saved. Do not preserve " \
+         "the undo/redo system for when the spreadsheet is saved. Select the folder to save the file in. "
 
-stories = pre_process_story(story)
-
-# Generate comaprison coefficient for all sentences
-story_dict = function_decision(stories)
-for thing in story_dict:
-    print(thing)
-
-# Threshold coefficient
-coefficient = 0.65
-processing_dict = {}
-single_dict = {}
-
-# Split sentences bases on threshold
-for grade in story_dict:
-    if grade > coefficient:
-        processing_dict[grade] = story_dict[grade]
-    else:
-        single_dict[grade] = story_dict[grade]
-
-for thing in processing_dict:
-    print(thing)
-
-# Generate new sentence
-new_story_set = set()
-
-# Use original sentence to keep the order of the sentences
-for thing in processing_dict.values():
-    new_story_set.add(thing[0])
-    new_story_set.add(thing[1])
-
-print(new_story_set)
-
-# Add sentences together to form new user story
-result_story = ""
-for thing in stories:
-    if thing in new_story_set:
-        result_story += thing + ". "
-
-print(result_story)
+stories = pre_process_story(story2)
 
 
+# Used story set
+used_story_set = set()
 
+
+def function_decision_helper(coefficient):
+    # Generate comparison coefficient for all sentences
+    story_dict = function_decision(stories)
+
+    # Result list
+    result = []
+
+    # Threshold coefficient
+    processing_dict = {}
+    single_dict = {}
+
+    # Split sentences bases on threshold
+    for grade in story_dict:
+        if grade > coefficient:
+            processing_dict[grade] = story_dict[grade]
+        else:
+            single_dict[grade] = story_dict[grade]
+
+    # for value in single_dict.values():
+    #     print("The result single story is: " + value[0])
+
+    # The list that holds all the pairs sentences that need to processing
+    # This list can hold duplicates of first sentences
+    processing_list = []
+
+    for value in processing_dict.values():
+        processing_list.append(value)
+
+    # Iterate through the processing list
+    while processing_list:
+        # Current new story set
+        new_story_set = set()
+
+        # Add all vectors that are related to each other
+        for item in processing_list:
+            first_vector = item[0]
+            second_vector = item[1]
+            if not new_story_set:
+                # If the set is empty then just add the two vectors
+                new_story_set.add(first_vector)
+                new_story_set.add(second_vector)
+
+            if first_vector in new_story_set or second_vector in new_story_set:
+                # If the set is not empty then only add if one of the vector appears
+                # Maintain the relationship between vector pairs
+                new_story_set.add(first_vector)
+                new_story_set.add(second_vector)
+
+                # Add to the used story set to keep track of the stories
+                used_story_set.add(first_vector)
+                used_story_set.add(second_vector)
+
+        # Remove all trace of vectors already been used
+        # Loop through the current new story set
+        for sentence_in_new_story_set in new_story_set:
+            # Loop through the processing list
+            for sentences_pair in processing_list:
+                first_sentence = sentences_pair[0]
+                second_sentence = sentences_pair[1]
+                if first_sentence == sentence_in_new_story_set or second_sentence == sentence_in_new_story_set:
+                    processing_list.remove(sentences_pair)
+
+        # Add sentences together to form new user story
+        result_story = ""
+        for item in stories:
+            if item in new_story_set:
+                result_story += item + ". \n"
+
+        print("The result story is: " + result_story)
+
+        result.append(result_story)
+
+    return result
+
+
+my_result = function_decision_helper(0.70)
+
+single_story_list = set(stories) - used_story_set
+
+for item in single_story_list:
+    print(item + "\n")
+
+# print(my_result)
