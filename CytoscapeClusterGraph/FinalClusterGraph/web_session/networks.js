9ccb407230cc14d79d2d3eb89542df3a3c0ad4f3
Phong Bach <phongnghinhbach@gmail.com>
2020-12-10 23:59:14 -0800

    Fix arrow direction for graph

--- CytoscapeClusterGraph/FinalClusterGraph/web_session/networks.js
+++ CytoscapeClusterGraph/FinalClusterGraph/web_session/networks.js
@@ -5,227 +5,32 @@ var networks = {"Network": {
   "data" : {
     "shared_name" : "Network",
     "name" : "Network",
-    "SUID" : 285,
+    "SUID" : 207,
     "__Annotations" : [ ],
     "selected" : true
   },
   "elements" : {
     "nodes" : [ {
       "data" : {
-        "id" : "310",
-        "shared_name" : "Issue 12",
-        "name" : "AI4-107",
-        "SUID" : 310,
-        "assignee" : "no",
-        "type" : "epic",
-        "selected" : false
-      },
-      "position" : {
-        "x" : -461.20804417282864,
-        "y" : -84.36123820070354
-      },
-      "selected" : false
-    }, {
-      "data" : {
-        "id" : "309",
-        "shared_name" : "Issue 9",
-        "name" : "AI4-6",
-        "SUID" : 309,
-        "assignee" : "no",
-        "type" : "story",
-        "selected" : false
-      },
-      "position" : {
-        "x" : -322.1032468095474,
-        "y" : -21.334718425312907
-      },
-      "selected" : false
-    }, {
-      "data" : {
-        "id" : "308",
-        "shared_name" : "Issue 10",
-        "name" : "AI4-38",
-        "SUID" : 308,
-        "assignee" : "no",
-        "type" : "epic",
-        "selected" : false
-      },
-      "position" : {
-        "x" : -399.97687351853176,
-        "y" : -233.09506388429728
-      },
-      "selected" : false
-    }, {
-      "data" : {
-        "id" : "307",
-        "shared_name" : "Issue 11",
-        "name" : "AI4-105",
-        "SUID" : 307,
-        "assignee" : "no",
-        "type" : "epic",
-        "selected" : false
-      },
-      "position" : {
-        "x" : -243.52490818650054,
-        "y" : -148.60171671632853
-      },
-      "selected" : false
-    }, {
-      "data" : {
-        "id" : "306",
-        "shared_name" : "Issue 8",
-        "name" : "AI4-102",
-        "SUID" : 306,
-        "assignee" : "no",
-        "type" : "story",
-        "selected" : false
-      },
-      "position" : {
-        "x" : -282.34224522263344,
-        "y" : 198.03316860864672
-      },
-      "selected" : false
-    }, {
-      "data" : {
-        "id" : "305",
-        "shared_name" : "Issue 7",
-        "name" : "AI4-8",
-        "SUID" : 305,
-        "assignee" : "no",
-        "type" : "epic",
-        "selected" : false
-      },
-      "position" : {
-        "x" : -462.0665751521561,
-        "y" : 107.15631988176233
-      },
-      "selected" : false
-    }, {
-      "data" : {
-        "id" : "304",
-        "shared_name" : "Issue 6",
-        "name" : "AI4-101",
-        "SUID" : 304,
-        "assignee" : "no",
-        "type" : "story",
-        "selected" : false
-      },
-      "position" : {
-        "x" : -410.71968662888344,
-        "y" : 299.6391256398967
-      },
-      "selected" : false
-    }, {
-      "data" : {
-        "id" : "303",
-        "shared_name" : "Issue 5",
-        "name" : "AI4-99",
-        "SUID" : 303,
-        "assignee" : "no",
-        "type" : "task",
-        "selected" : false
-      },
-      "position" : {
-        "x" : -93.03655064255523,
-        "y" : -37.55245371584025
-      },
-      "selected" : false
-    }, {
-      "data" : {
-        "id" : "302",
-        "shared_name" : "Issue 4",
-        "name" : "AI4-78",
-        "SUID" : 302,
-        "assignee" : "no",
-        "type" : "story",
-        "selected" : false
-      },
-      "position" : {
-        "x" : 35.34089076369477,
-        "y" : -139.15841074709024
-      },
-      "selected" : false
-    }, {
-      "data" : {
-        "id" : "301",
-        "shared_name" : "Issue 2",
-        "name" : "AI4-98",
-        "SUID" : 301,
-        "assignee" : "no",
-        "type" : "task",
-        "selected" : false
-      },
-      "position" : {
-        "x" : -61.775686384742784,
-        "y" : 282.5255264660269
-      },
-      "selected" : false
-    }, {
-      "data" : {
-        "id" : "300",
-        "shared_name" : "Issue 3",
-        "name" : "AI4-97",
-        "SUID" : 300,
-        "assignee" : "no",
-        "type" : "story",
-        "selected" : false
-      },
-      "position" : {
-        "x" : -143.52490818650054,
-        "y" : -233.86001498687793
-      },
-      "selected" : false
-    }, {
-      "data" : {
-        "id" : "299",
-        "shared_name" : "Issue 1",
-        "name" : "AI4-100",
-        "SUID" : 299,
-        "assignee" : "no",
-        "type" : "task",
-        "selected" : false
-      },
-      "position" : {
-        "x" : -182.3422452226334,
-        "y" : 104.09651547143967
-      },
-      "selected" : false
-    }, {
-      "data" : {
-        "id" : "298",
-        "shared_name" : "Nain",
-        "name" : "NG",
-        "SUID" : 298,
+        "id" : "232",
+        "shared_name" : "Emily",
+        "name" : "EC",
+        "SUID" : 232,
         "assignee" : "yes",
         "type" : "assignee",
         "selected" : false
       },
       "position" : {
-        "x" : -392.21190464646156,
-        "y" : 191.14924221704516
+        "x" : -74.52876866013335,
+        "y" : -146.0423371386918
       },
       "selected" : false
     }, {
       "data" : {
-        "id" : "297",
-        "shared_name" : "Aric",
-        "name" : "AM",
-        "SUID" : 297,
-        "assignee" : "yes",
-        "type" : "assignee",
-        "selected" : false
-      },
-      "position" : {
-        "x" : -356.09659397751614,
-        "y" : -128.10174723390665
-      },
-      "selected" : false
-    }, {
-      "data" : {
-        "id" : "296",
+        "id" : "231",
         "shared_name" : "Phong",
         "name" : "PB",
-        "SUID" : 296,
+        "SUID" : 231,
         "assignee" : "yes",
         "type" : "assignee",
         "selected" : false
@@ -237,172 +42,367 @@ var networks = {"Network": {
       "selected" : false
     }, {
       "data" : {
-        "id" : "295",
-        "shared_name" : "Emily",
-        "name" : "EC",
-        "SUID" : 295,
+        "id" : "230",
+        "shared_name" : "Aric",
+        "name" : "AM",
+        "SUID" : 230,
         "assignee" : "yes",
         "type" : "assignee",
         "selected" : false
       },
       "position" : {
-        "x" : -74.52876866013335,
-        "y" : -146.0423371386918
+        "x" : -356.09659397751614,
+        "y" : -128.10174723390665
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "229",
+        "shared_name" : "Nain",
+        "name" : "NG",
+        "SUID" : 229,
+        "assignee" : "yes",
+        "type" : "assignee",
+        "selected" : false
+      },
+      "position" : {
+        "x" : -392.21190464646156,
+        "y" : 191.14924221704516
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "228",
+        "shared_name" : "Issue 1",
+        "name" : "AI4-100",
+        "SUID" : 228,
+        "assignee" : "no",
+        "type" : "task",
+        "selected" : false
+      },
+      "position" : {
+        "x" : -182.3422452226334,
+        "y" : 104.09651547143967
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "227",
+        "shared_name" : "Issue 3",
+        "name" : "AI4-97",
+        "SUID" : 227,
+        "assignee" : "no",
+        "type" : "story",
+        "selected" : false
+      },
+      "position" : {
+        "x" : -144.66207608108337,
+        "y" : -233.86001498687793
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "226",
+        "shared_name" : "Issue 2",
+        "name" : "AI4-98",
+        "SUID" : 226,
+        "assignee" : "no",
+        "type" : "task",
+        "selected" : false
+      },
+      "position" : {
+        "x" : -61.775686384742784,
+        "y" : 282.5255264660269
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "225",
+        "shared_name" : "Issue 4",
+        "name" : "AI4-78",
+        "SUID" : 225,
+        "assignee" : "no",
+        "type" : "story",
+        "selected" : false
+      },
+      "position" : {
+        "x" : 35.34089076369477,
+        "y" : -139.15841074709024
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "224",
+        "shared_name" : "Issue 5",
+        "name" : "AI4-99",
+        "SUID" : 224,
+        "assignee" : "no",
+        "type" : "task",
+        "selected" : false
+      },
+      "position" : {
+        "x" : -95.31088643172095,
+        "y" : -36.415285821257406
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "223",
+        "shared_name" : "Issue 6",
+        "name" : "AI4-101",
+        "SUID" : 223,
+        "assignee" : "no",
+        "type" : "story",
+        "selected" : false
+      },
+      "position" : {
+        "x" : -410.71968662888344,
+        "y" : 299.6391256398967
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "222",
+        "shared_name" : "Issue 7",
+        "name" : "AI4-8",
+        "SUID" : 222,
+        "assignee" : "no",
+        "type" : "epic",
+        "selected" : false
+      },
+      "position" : {
+        "x" : -462.0665751521561,
+        "y" : 107.15631988176233
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "221",
+        "shared_name" : "Issue 8",
+        "name" : "AI4-102",
+        "SUID" : 221,
+        "assignee" : "no",
+        "type" : "story",
+        "selected" : false
+      },
+      "position" : {
+        "x" : -281.2050773280506,
+        "y" : 198.03316860864672
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "220",
+        "shared_name" : "Issue 11",
+        "name" : "AI4-105",
+        "SUID" : 220,
+        "assignee" : "no",
+        "type" : "epic",
+        "selected" : false
+      },
+      "position" : {
+        "x" : -243.52490818650054,
+        "y" : -148.60171671632853
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "219",
+        "shared_name" : "Issue 10",
+        "name" : "AI4-38",
+        "SUID" : 219,
+        "assignee" : "no",
+        "type" : "epic",
+        "selected" : false
+      },
+      "position" : {
+        "x" : -399.97687351853176,
+        "y" : -233.09506388429728
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "218",
+        "shared_name" : "Issue 9",
+        "name" : "AI4-6",
+        "SUID" : 218,
+        "assignee" : "no",
+        "type" : "story",
+        "selected" : false
+      },
+      "position" : {
+        "x" : -323.2404147041302,
+        "y" : -21.334718425312907
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "217",
+        "shared_name" : "Issue 12",
+        "name" : "AI4-107",
+        "SUID" : 217,
+        "assignee" : "no",
+        "type" : "epic",
+        "selected" : false
+      },
+      "position" : {
+        "x" : -463.48237996199435,
+        "y" : -84.36123820070354
       },
       "selected" : false
     } ],
     "edges" : [ {
       "data" : {
-        "id" : "322",
-        "source" : "310",
-        "target" : "297",
-        "shared_name" : "Issue 12 (interacts with) Aric",
-        "name" : "Issue 12 (interacts with) Aric",
+        "id" : "799",
+        "source" : "232",
+        "target" : "224",
+        "shared_name" : "Emily (interacts with) Issue 5",
+        "name" : "Emily (interacts with) Issue 5",
         "interaction" : "interacts with",
-        "SUID" : 322,
+        "SUID" : 799,
         "shared_interaction" : "interacts with",
         "selected" : false
       },
       "selected" : false
     }, {
       "data" : {
-        "id" : "321",
-        "source" : "309",
-        "target" : "297",
-        "shared_name" : "Issue 9 (interacts with) Aric",
-        "name" : "Issue 9 (interacts with) Aric",
+        "id" : "797",
+        "source" : "232",
+        "target" : "227",
+        "shared_name" : "Emily (interacts with) Issue 3",
+        "name" : "Emily (interacts with) Issue 3",
         "interaction" : "interacts with",
-        "SUID" : 321,
+        "SUID" : 797,
         "shared_interaction" : "interacts with",
         "selected" : false
       },
       "selected" : false
     }, {
       "data" : {
-        "id" : "320",
-        "source" : "307",
-        "target" : "297",
-        "shared_name" : "Issue 11 (interacts with) Aric",
-        "name" : "Issue 11 (interacts with) Aric",
+        "id" : "795",
+        "source" : "232",
+        "target" : "225",
+        "shared_name" : "Emily (interacts with) Issue 4",
+        "name" : "Emily (interacts with) Issue 4",
         "interaction" : "interacts with",
-        "SUID" : 320,
+        "SUID" : 795,
         "shared_interaction" : "interacts with",
         "selected" : false
       },
       "selected" : false
     }, {
       "data" : {
-        "id" : "319",
-        "source" : "306",
-        "target" : "298",
-        "shared_name" : "Node 10 (interacts with) Nain",
-        "name" : "Node 10 (interacts with) Nain",
-        "interaction" : "interacts with",
-        "SUID" : 319,
-        "shared_interaction" : "interacts with",
-        "selected" : false
-      },
-      "selected" : false
-    }, {
-      "data" : {
-        "id" : "318",
-        "source" : "305",
-        "target" : "298",
-        "shared_name" : "Node 9 (interacts with) Nain",
-        "name" : "Node 9 (interacts with) Nain",
-        "interaction" : "interacts with",
-        "SUID" : 318,
-        "shared_interaction" : "interacts with",
-        "selected" : false
-      },
-      "selected" : false
-    }, {
-      "data" : {
-        "id" : "317",
-        "source" : "304",
-        "target" : "298",
-        "shared_name" : "Node 8 (interacts with) Nain",
-        "name" : "Node 8 (interacts with) Nain",
-        "interaction" : "interacts with",
-        "SUID" : 317,
-        "shared_interaction" : "interacts with",
-        "selected" : false
-      },
-      "selected" : false
-    }, {
-      "data" : {
-        "id" : "316",
-        "source" : "303",
-        "target" : "295",
-        "shared_name" : "Issue 5 (interacts with) Emily",
-        "name" : "Issue 5 (interacts with) Emily",
-        "interaction" : "interacts with",
-        "SUID" : 316,
-        "shared_interaction" : "interacts with",
-        "selected" : false
-      },
-      "selected" : false
-    }, {
-      "data" : {
-        "id" : "315",
-        "source" : "302",
-        "target" : "295",
-        "shared_name" : "Issue 4 (interacts with) Emily",
-        "name" : "Issue 4 (interacts with) Emily",
-        "interaction" : "interacts with",
-        "SUID" : 315,
-        "shared_interaction" : "interacts with",
-        "selected" : false
-      },
-      "selected" : false
-    }, {
-      "data" : {
-        "id" : "314",
-        "source" : "300",
-        "target" : "295",
-        "shared_name" : "Issue 3 (interacts with) Emily",
-        "name" : "Issue 3 (interacts with) Emily",
-        "interaction" : "interacts with",
-        "SUID" : 314,
-        "shared_interaction" : "interacts with",
-        "selected" : false
-      },
-      "selected" : false
-    }, {
-      "data" : {
-        "id" : "313",
-        "source" : "297",
-        "target" : "308",
-        "shared_name" : "Aric (interacts with) Issue 10",
-        "name" : "Aric (interacts with) Issue 10",
-        "interaction" : "interacts with",
-        "SUID" : 313,
-        "shared_interaction" : "interacts with",
-        "selected" : false
-      },
-      "selected" : false
-    }, {
-      "data" : {
-        "id" : "312",
-        "source" : "296",
-        "target" : "301",
-        "shared_name" : "Phong (interacts with) Issue 2",
-        "name" : "Phong (interacts with) Issue 2",
-        "interaction" : "interacts with",
-        "SUID" : 312,
-        "shared_interaction" : "interacts with",
-        "selected" : false
-      },
-      "selected" : false
-    }, {
-      "data" : {
-        "id" : "311",
-        "source" : "296",
-        "target" : "299",
+        "id" : "244",
+        "source" : "231",
+        "target" : "228",
         "shared_name" : "Phong (interacts with) Issue 1",
         "name" : "Phong (interacts with) Issue 1",
         "interaction" : "interacts with",
-        "SUID" : 311,
+        "SUID" : 244,
+        "shared_interaction" : "interacts with",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "243",
+        "source" : "231",
+        "target" : "226",
+        "shared_name" : "Phong (interacts with) Issue 2",
+        "name" : "Phong (interacts with) Issue 2",
+        "interaction" : "interacts with",
+        "SUID" : 243,
+        "shared_interaction" : "interacts with",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "811",
+        "source" : "230",
+        "target" : "217",
+        "shared_name" : "Aric (interacts with) Issue 12",
+        "name" : "Aric (interacts with) Issue 12",
+        "interaction" : "interacts with",
+        "SUID" : 811,
+        "shared_interaction" : "interacts with",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "809",
+        "source" : "230",
+        "target" : "218",
+        "shared_name" : "Aric (interacts with) Issue 9",
+        "name" : "Aric (interacts with) Issue 9",
+        "interaction" : "interacts with",
+        "SUID" : 809,
+        "shared_interaction" : "interacts with",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "807",
+        "source" : "230",
+        "target" : "220",
+        "shared_name" : "Aric (interacts with) Issue 11",
+        "name" : "Aric (interacts with) Issue 11",
+        "interaction" : "interacts with",
+        "SUID" : 807,
+        "shared_interaction" : "interacts with",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "242",
+        "source" : "230",
+        "target" : "219",
+        "shared_name" : "Aric (interacts with) Issue 10",
+        "name" : "Aric (interacts with) Issue 10",
+        "interaction" : "interacts with",
+        "SUID" : 242,
+        "shared_interaction" : "interacts with",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "805",
+        "source" : "229",
+        "target" : "223",
+        "shared_name" : "Nain (interacts with) Issue 6",
+        "name" : "Nain (interacts with) Issue 6",
+        "interaction" : "interacts with",
+        "SUID" : 805,
+        "shared_interaction" : "interacts with",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "803",
+        "source" : "229",
+        "target" : "222",
+        "shared_name" : "Nain (interacts with) Issue 7",
+        "name" : "Nain (interacts with) Issue 7",
+        "interaction" : "interacts with",
+        "SUID" : 803,
+        "shared_interaction" : "interacts with",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "801",
+        "source" : "229",
+        "target" : "221",
+        "shared_name" : "Nain (interacts with) Issue 8",
+        "name" : "Nain (interacts with) Issue 8",
+        "interaction" : "interacts with",
+        "SUID" : 801,
         "shared_interaction" : "interacts with",
         "selected" : false
       },
--- CytoscapeClusterGraph/FinalClusterGraph/web_session/styles.js
+++ CytoscapeClusterGraph/FinalClusterGraph/web_session/styles.js
@@ -6,37 +6,22 @@ var styles = [ {
   "style" : [ {
     "selector" : "node",
     "css" : {
-      "width" : 70.0,
-      "text-valign" : "center",
-      "text-halign" : "center",
-      "height" : 40.0,
-      "color" : "rgb(0,0,0)",
       "background-opacity" : 1.0,
-      "shape" : "roundrectangle",
       "font-family" : "Dialog.plain",
       "font-weight" : "normal",
-      "border-width" : 0.0,
-      "border-opacity" : 1.0,
-      "background-color" : "rgb(14,180,227)",
-      "border-color" : "rgb(255,255,255)",
       "text-opacity" : 1.0,
+      "shape" : "roundrectangle",
+      "border-width" : 0.0,
+      "text-valign" : "center",
+      "text-halign" : "center",
+      "border-opacity" : 1.0,
+      "border-color" : "rgb(255,255,255)",
+      "width" : 70.0,
+      "background-color" : "rgb(14,180,227)",
+      "color" : "rgb(0,0,0)",
       "font-size" : 12,
+      "height" : 40.0,
       "content" : "data(name)"
-    }
-  }, {
-    "selector" : "node[assignee = '']",
-    "css" : {
-      "border-color" : "rgb(255,255,255)"
-    }
-  }, {
-    "selector" : "node[assignee = 'no']",
-    "css" : {
-      "border-color" : "rgb(255,255,255)"
-    }
-  }, {
-    "selector" : "node[assignee = 'yes']",
-    "css" : {
-      "border-color" : "rgb(255,255,255)"
     }
   }, {
     "selector" : "node[assignee = '']",
@@ -47,66 +32,6 @@ var styles = [ {
     "selector" : "node[assignee = 'yes']",
     "css" : {
       "height" : 60.0
-    }
-  }, {
-    "selector" : "node[assignee = '']",
-    "css" : {
-      "shape" : "roundrectangle"
-    }
-  }, {
-    "selector" : "node[assignee = 'no']",
-    "css" : {
-      "shape" : "roundrectangle"
-    }
-  }, {
-    "selector" : "node[assignee = 'yes']",
-    "css" : {
-      "shape" : "ellipse"
-    }
-  }, {
-    "selector" : "node[assignee = '']",
-    "css" : {
-      "color" : "rgb(0,0,0)"
-    }
-  }, {
-    "selector" : "node[assignee = 'yes']",
-    "css" : {
-      "color" : "rgb(255,255,255)"
-    }
-  }, {
-    "selector" : "node[assignee = '']",
-    "css" : {
-      "width" : 100.0
-    }
-  }, {
-    "selector" : "node[assignee = 'no']",
-    "css" : {
-      "width" : 100.0
-    }
-  }, {
-    "selector" : "node[assignee = 'yes']",
-    "css" : {
-      "width" : 60.0
-    }
-  }, {
-    "selector" : "node[assignee = '']",
-    "css" : {
-      "font-size" : 17
-    }
-  }, {
-    "selector" : "node[assignee = 'no']",
-    "css" : {
-      "font-size" : 15
-    }
-  }, {
-    "selector" : "node[assignee = 'yes']",
-    "css" : {
-      "font-size" : 28
-    }
-  }, {
-    "selector" : "node[assignee = 'yes']",
-    "css" : {
-      "border-width" : 0.0
     }
   }, {
     "selector" : "node[type = 'task']",
@@ -129,77 +54,152 @@ var styles = [ {
       "background-color" : "rgb(91,196,29)"
     }
   }, {
-    "selector" : "node[ id = '295' ]",
+    "selector" : "node[assignee = '']",
+    "css" : {
+      "color" : "rgb(0,0,0)"
+    }
+  }, {
+    "selector" : "node[assignee = 'yes']",
+    "css" : {
+      "color" : "rgb(255,255,255)"
+    }
+  }, {
+    "selector" : "node[assignee = '']",
+    "css" : {
+      "border-color" : "rgb(255,255,255)"
+    }
+  }, {
+    "selector" : "node[assignee = 'no']",
+    "css" : {
+      "border-color" : "rgb(255,255,255)"
+    }
+  }, {
+    "selector" : "node[assignee = 'yes']",
+    "css" : {
+      "border-color" : "rgb(255,255,255)"
+    }
+  }, {
+    "selector" : "node[assignee = '']",
+    "css" : {
+      "font-size" : 17
+    }
+  }, {
+    "selector" : "node[assignee = 'no']",
+    "css" : {
+      "font-size" : 15
+    }
+  }, {
+    "selector" : "node[assignee = 'yes']",
+    "css" : {
+      "font-size" : 28
+    }
+  }, {
+    "selector" : "node[assignee = '']",
+    "css" : {
+      "width" : 100.0
+    }
+  }, {
+    "selector" : "node[assignee = 'no']",
+    "css" : {
+      "width" : 100.0
+    }
+  }, {
+    "selector" : "node[assignee = 'yes']",
+    "css" : {
+      "width" : 60.0
+    }
+  }, {
+    "selector" : "node[assignee = '']",
+    "css" : {
+      "shape" : "roundrectangle"
+    }
+  }, {
+    "selector" : "node[assignee = 'no']",
+    "css" : {
+      "shape" : "roundrectangle"
+    }
+  }, {
+    "selector" : "node[assignee = 'yes']",
+    "css" : {
+      "shape" : "ellipse"
+    }
+  }, {
+    "selector" : "node[assignee = 'yes']",
+    "css" : {
+      "border-width" : 0.0
+    }
+  }, {
+    "selector" : "node[ id = '224' ]",
     "css" : {
       "border-color" : "rgb(0,0,0)"
     }
   }, {
-    "selector" : "node[ id = '296' ]",
+    "selector" : "node[ id = '225' ]",
     "css" : {
       "border-color" : "rgb(0,0,0)"
     }
   }, {
-    "selector" : "node[ id = '297' ]",
+    "selector" : "node[ id = '226' ]",
     "css" : {
       "border-color" : "rgb(0,0,0)"
     }
   }, {
-    "selector" : "node[ id = '298' ]",
+    "selector" : "node[ id = '227' ]",
     "css" : {
       "border-color" : "rgb(0,0,0)"
     }
   }, {
-    "selector" : "node[ id = '299' ]",
+    "selector" : "node[ id = '228' ]",
     "css" : {
       "border-color" : "rgb(0,0,0)"
     }
   }, {
-    "selector" : "node[ id = '300' ]",
+    "selector" : "node[ id = '229' ]",
     "css" : {
       "border-color" : "rgb(0,0,0)"
     }
   }, {
-    "selector" : "node[ id = '301' ]",
+    "selector" : "node[ id = '230' ]",
     "css" : {
       "border-color" : "rgb(0,0,0)"
     }
   }, {
-    "selector" : "node[ id = '302' ]",
+    "selector" : "node[ id = '231' ]",
     "css" : {
       "border-color" : "rgb(0,0,0)"
     }
   }, {
-    "selector" : "node[ id = '303' ]",
+    "selector" : "node[ id = '232' ]",
     "css" : {
       "border-color" : "rgb(0,0,0)"
     }
   }, {
-    "selector" : "node[ id = '304' ]",
+    "selector" : "node[ id = '218' ]",
     "css" : {
       "border-color" : "rgb(0,0,0)"
     }
   }, {
-    "selector" : "node[ id = '305' ]",
+    "selector" : "node[ id = '219' ]",
     "css" : {
       "border-color" : "rgb(0,0,0)"
     }
   }, {
-    "selector" : "node[ id = '306' ]",
+    "selector" : "node[ id = '220' ]",
     "css" : {
       "border-color" : "rgb(0,0,0)"
     }
   }, {
-    "selector" : "node[ id = '307' ]",
+    "selector" : "node[ id = '221' ]",
     "css" : {
       "border-color" : "rgb(0,0,0)"
     }
   }, {
-    "selector" : "node[ id = '308' ]",
+    "selector" : "node[ id = '222' ]",
     "css" : {
       "border-color" : "rgb(0,0,0)"
     }
   }, {
-    "selector" : "node[ id = '309' ]",
+    "selector" : "node[ id = '223' ]",
     "css" : {
       "border-color" : "rgb(0,0,0)"
     }
@@ -211,20 +211,20 @@ var styles = [ {
   }, {
     "selector" : "edge",
     "css" : {
-      "content" : "",
-      "target-arrow-color" : "rgb(0,0,0)",
       "source-arrow-color" : "rgb(0,0,0)",
+      "line-color" : "rgb(102,102,102)",
       "source-arrow-shape" : "none",
+      "text-opacity" : 1.0,
+      "line-style" : "solid",
       "opacity" : 1.0,
-      "width" : 3.0,
-      "target-arrow-shape" : "none",
-      "font-size" : 10,
+      "target-arrow-color" : "rgb(0,0,0)",
+      "target-arrow-shape" : "triangle",
       "font-family" : "Dialog.plain",
       "font-weight" : "normal",
-      "color" : "rgb(0,0,0)",
-      "line-style" : "solid",
-      "line-color" : "rgb(102,102,102)",
-      "text-opacity" : 1.0
+      "font-size" : 10,
+      "content" : "",
+      "width" : 3.0,
+      "color" : "rgb(0,0,0)"
     }
   }, {
     "selector" : "edge:selected",
