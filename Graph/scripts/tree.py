349e83f1bf77c79ab78e6e426b963d8107763113
Emily <emily.cawlfield@wsu.edu>
2021-02-19 18:25:00 -0800

    edges

--- Graph/scripts/tree.py
+++ Graph/scripts/tree.py
@@ -1,6 +1,7 @@
 import json
 
 nodes = []
+edges = []
 
 # Main source: https://isquared.digital/blog/2020-03-24-viz-tools-pt2-2/
 with open('IssueQueryResults.json') as f:
@@ -46,6 +47,23 @@ with open('IssueQueryResults.json') as f:
             "grabbable": True  # we can grab and move the node
         }
         nodes.append(node)
+        edge = {
+            "data": {
+                "source": str(1),  # the source node id (edge comes from this node)(selected issue)
+                "target": str(0),  # the target node id (edge goes to this node)(parent issue)
+                "directed": True,
+                "id": "e" + str(1)
+            },
+            "position": {},  # the initial position is not known
+            "group": "edges",  # it belongs in the group of edges
+            "removed": False,
+            "selected": False,  # the edge is not selected
+            "selectable": True,  # we can select the node
+            "locked": False,  # the edge position is not immutable
+            "grabbable": True,  # we can grab and move the node
+            "directed": True  # the edge is directed
+        }
+        edges.append(edge)
 
     # Loop to get linked/blocking issues
     issueLinks = issueData.get("fields").get("issuelinks") # issuelinks is a list of dictionaries, not a single dictionary
@@ -66,4 +84,21 @@ with open('IssueQueryResults.json') as f:
             "grabbable": True  # we can grab and move the node
         }
         nodes.append(node)
-print(nodes)
+        edge = {
+            "data": {
+                "source": str(i + 2),  # the source node id (edge comes from this node)(current blocking issue)
+                "target": str(1),  # the target node id (edge goes to this node)(selected issue)
+                "directed": True,
+                "id": "e" + str(i + 2)
+            },
+            "position": {},  # the initial position is not known
+            "group": "edges",  # it belongs in the group of edges
+            "removed": False,
+            "selected": False,  # the edge is not selected
+            "selectable": True,  # we can select the node
+            "locked": False,  # the edge position is not immutable
+            "grabbable": True,  # we can grab and move the node
+            "directed": True  # the edge is directed
+        }
+        edges.append(edge)
+   
