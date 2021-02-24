8fc9bb51ef172e2a10f5919b72726fe4d9589b12
Emily <emily.cawlfield@wsu.edu>
2021-02-24 15:55:19 -0800

    graph switching implemented; some cluster graph files yet to be added

--- Graph/scripts/util.js
+++ Graph/scripts/util.js
@@ -1,54 +1,14 @@
-var graphType = "tree";
-
 function switchGraph() {
 	var currentText = document.getElementById("switchGraphs").innerHTML;
-	
-	//$.getScript('styles/styles.json', function() {});
-	
-	if (currentText === "Click to view Developer Cluster") {
-		document.getElementById("switchGraphs").innerHTML="Click to view Issue Tree";
-		//loadClusterGraph() 
+		
+	if (currentText === "View Developer Cluster") {
+		document.getElementById("switchGraphs").innerHTML="View Issue Tree";
+		document.getElementById("cyTr").setAttribute('style', 'display: none;')
+		document.getElementById("cyCl").setAttribute('style', 'display: inline')
 	}
 	else {
-		document.getElementById("switchGraphs").innerHTML="Click to view Developer Cluster"; 
-		//loadTreeGraph()
+		document.getElementById("switchGraphs").innerHTML="View Developer Cluster"; 
+		document.getElementById("cyTr").setAttribute('style', 'display: inline')
+		document.getElementById("cyCl").setAttribute('style', 'display: none;')
 	}
 }
-
-function loadTreeGraph() {
-	/* cy.json({
-		elements: treeNetwork, 
-		style: treeStyle, 
-		layout: treeLayout,
-		zoomingEnabled: true
-	}) */
-}
-
-function loadClusterGraph() {
-	/* cy = document.getElementById('cy')
-	cy.json({
-		style: testStyle, 
-		elements: testNetwork, 
-		layout: clusterLayout, 
-		style: treeStyle, //temp
-		zoomingEnabled: true,
-	}) */
-}
-
-/* function getIssueFromURLParameters() {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-
-	var currentText = document.getElementById("switchGraphs").innerHTML;
-	
-	if(currentText === "Click to view Developer Cluster") {
-		graphType = "tree";
-	}
-	else{
-		graphType = "cluster";
-	}
-
-	var graphToGrab = urlParams.get('issueKey') + graphType;
-	
-    return graphToGrab;
-} */
