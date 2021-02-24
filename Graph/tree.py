2c88754888b221a36fc7e430e28e4a437539673c
Emily <emily.cawlfield@wsu.edu>
2021-02-23 22:46:05 -0800

    moved querying into network file generation script; added layout running

--- Graph/tree.py
+++ Graph/tree.py
@@ -1,5 +1,17 @@
 import json
 
+from atlassian import Jira
+
+jira = Jira(
+    url='https://playingabout.atlassian.net/',
+    username='aric.monary@wsu.edu',
+    password='NCx7f7ZWle5yfwS3DB8JBCEA')
+
+# expect issue key as input from Ajax response
+with open('data/IssueQueryResults.json', 'w') as f:   
+        f.write(json.dumps(jira.issue('AI4-97')))
+
+
 nodes = []
 edges = []
 
