8393909d5d8260b168b3d12636c243d156932c90
ngalvan <ngalvan@eecs.wsu.edu>
2021-02-24 21:41:51 -0800

    Folder for Jira Cloud Server

---
+++ CapstoneJiraCloud/views/Graph/networks.js
@@ -0,0 +1,275 @@
+var networks = {"Sheet1_2": {
+  "format_version" : "1.0",
+  "generated_by" : "cytoscape-3.8.2",
+  "target_cytoscapejs_version" : "~2.1",
+  "data" : {
+    "shared_name" : "Sheet1_2",
+    "name" : "Sheet1_2",
+    "SUID" : 217,
+    "__Annotations" : [ "edgeThickness=1.0|parent=91e45c59-5604-437c-ad40-a2cbd1b1271a|canvas=foreground|fillOpacity=100.0|type=org.cytoscape.view.presentation.annotations.ShapeAnnotation|uuid=d84dc62c-1999-4fb4-97c8-f8d1a6f21f63|fillColor=-15813405|shapeType=RECTANGLE|edgeColor=-16777216|edgeOpacity=100.0|name=Shape 1|x=-244.55474722974165|width=80.0|y=211.94788430137461|z=1|height=35.0", "parent=91e45c59-5604-437c-ad40-a2cbd1b1271a|canvas=foreground|color=-16777216|type=org.cytoscape.view.presentation.annotations.TextAnnotation|fontStyle=0|uuid=eed89b26-3538-4cde-9e66-9d31d31566d6|fontFamily=SansSerif|name=Text|x=-144.5547472297417|y=234.44788430137456|z=2|fontSize=20|text=Story", "canvas=foreground|name=legend|x=-255.0|y=83.0|z=0|type=org.cytoscape.view.presentation.annotations.GroupAnnotation|uuid=91e45c59-5604-437c-ad40-a2cbd1b1271a|memberUUIDs=507d3d56-aa24-4a9e-866f-4d6bddbb455f,fa277b33-3703-47ea-a1fc-ea003a39009b,fba30a58-495d-4658-abab-258967de54dd,e7c4371d-a358-4cec-94c3-2d68195e3638,472aca64-4990-4563-bda7-fdf1d3fe20c4,7db5dfcb-fa81-4219-b8cf-26033dcf6468,d84dc62c-1999-4fb4-97c8-f8d1a6f21f63,eed89b26-3538-4cde-9e66-9d31d31566d6", "canvas=foreground|fontFamily=Serif|color=-16777216|name=legend|x=-242.76599797070674|y=48.61150963914298|z=9|fontSize=24|text=Legend|type=org.cytoscape.view.presentation.annotations.TextAnnotation|fontStyle=0|uuid=a6379f0b-d460-40ea-8c7a-cc79db166238", "edgeThickness=1.0|parent=91e45c59-5604-437c-ad40-a2cbd1b1271a|canvas=foreground|fillOpacity=100.0|type=org.cytoscape.view.presentation.annotations.ShapeAnnotation|uuid=472aca64-4990-4563-bda7-fdf1d3fe20c4|fillColor=-6710785|shapeType=RECTANGLE|edgeColor=-16777216|edgeOpacity=100.0|name=Shape 1|x=-244.55474722974165|width=80.0|y=166.94788430137496|z=3|height=35.0", "edgeThickness=1.0|parent=91e45c59-5604-437c-ad40-a2cbd1b1271a|canvas=foreground|fillOpacity=100.0|type=org.cytoscape.view.presentation.annotations.ShapeAnnotation|uuid=fa277b33-3703-47ea-a1fc-ea003a39009b|shapeType=RECTANGLE|edgeColor=-16777216|edgeOpacity=100.0|name=Shape 1|x=-254.55474722974176|width=166.0|y=111.94788430137487|z=4|height=155.0", "parent=91e45c59-5604-437c-ad40-a2cbd1b1271a|canvas=foreground|color=-16777216|type=org.cytoscape.view.presentation.annotations.TextAnnotation|fontStyle=0|uuid=7db5dfcb-fa81-4219-b8cf-26033dcf6468|fontFamily=SansSerif|name=Text|x=-144.5547472297417|y=189.44788430137496|z=5|fontSize=20|text=Epic", "parent=91e45c59-5604-437c-ad40-a2cbd1b1271a|canvas=foreground|color=-16777216|type=org.cytoscape.view.presentation.annotations.TextAnnotation|fontStyle=0|uuid=e7c4371d-a358-4cec-94c3-2d68195e3638|fontFamily=SansSerif|name=Text|x=-144.5547472297417|y=144.44788430137487|z=6|fontSize=20|text=Task", "edgeThickness=1.0|parent=91e45c59-5604-437c-ad40-a2cbd1b1271a|canvas=foreground|fillOpacity=100.0|type=org.cytoscape.view.presentation.annotations.ShapeAnnotation|uuid=fba30a58-495d-4658-abab-258967de54dd|fillColor=-10027162|shapeType=RECTANGLE|edgeColor=-16777216|edgeOpacity=100.0|name=Shape 1|x=-244.55474722974165|width=80.0|y=121.94788430137487|z=7|height=35.0", "parent=91e45c59-5604-437c-ad40-a2cbd1b1271a|canvas=foreground|color=-16777216|type=org.cytoscape.view.presentation.annotations.TextAnnotation|fontStyle=0|uuid=507d3d56-aa24-4a9e-866f-4d6bddbb455f|fontFamily=SansSerif|name=Node Fill Color|x=-254.55474722974176|y=83.94788430137487|z=8|fontSize=20|text=Node Fill Color" ],
+    "selected" : true
+  },
+  "elements" : {
+    "nodes" : [ {
+      "data" : {
+        "id" : "244",
+        "IssueType_B" : "Epic",
+        "name" : "AI4-105",
+        "SUID" : 244,
+        "href" : "https://playingabout.atlassian.net/browse/AI4-105",
+        "selected" : false
+      },
+      "position" : {
+        "x" : -75.74753302428235,
+        "y" : -134.92171823864555
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "242",
+        "name" : "AI4-100",
+        "SUID" : 242,
+        "href" : "https://playingabout.atlassian.net/browse/AI4-100",
+        "IssueType_A" : "Task",
+        "selected" : false
+      },
+      "position" : {
+        "x" : 52.25246697571765,
+        "y" : 66.07828176135445
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "240",
+        "name" : "AI4-99",
+        "SUID" : 240,
+        "href" : "https://playingabout.atlassian.net/browse/AI4-99",
+        "IssueType_A" : "Task",
+        "selected" : false
+      },
+      "position" : {
+        "x" : -11.747533024282347,
+        "y" : 66.07828176135445
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "238",
+        "name" : "AI4-98",
+        "SUID" : 238,
+        "href" : "https://playingabout.atlassian.net/browse/AI4-98",
+        "IssueType_A" : "Task",
+        "selected" : false
+      },
+      "position" : {
+        "x" : 116.25246697571765,
+        "y" : 66.07828176135445
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "236",
+        "IssueType_B" : "Story",
+        "name" : "AI4-97",
+        "SUID" : 236,
+        "href" : "https://playingabout.atlassian.net/browse/AI4-97",
+        "IssueType_A" : "Story",
+        "selected" : false
+      },
+      "position" : {
+        "x" : 52.25246697571765,
+        "y" : -0.9217182386455534
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "234",
+        "name" : "AI4-92",
+        "SUID" : 234,
+        "href" : "https://playingabout.atlassian.net/browse/AI4-92",
+        "IssueType_A" : "Story",
+        "selected" : false
+      },
+      "position" : {
+        "x" : 116.25246697571765,
+        "y" : -0.9217182386455534
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "232",
+        "name" : "AI4-91",
+        "SUID" : 232,
+        "href" : "https://playingabout.atlassian.net/browse/AI4-91",
+        "IssueType_A" : "Story",
+        "selected" : false
+      },
+      "position" : {
+        "x" : -150.77798006401113,
+        "y" : -2.024762942618427
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "230",
+        "name" : "AI4-90",
+        "SUID" : 230,
+        "href" : "https://playingabout.atlassian.net/browse/AI4-90",
+        "IssueType_A" : "Story",
+        "selected" : false
+      },
+      "position" : {
+        "x" : -75.74753302428235,
+        "y" : -0.9217182386455534
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "228",
+        "IssueType_B" : "Epic",
+        "name" : "AI4-88",
+        "SUID" : 228,
+        "href" : "https://playingabout.atlassian.net/browse/AI4-88",
+        "IssueType_A" : "Epic",
+        "selected" : false
+      },
+      "position" : {
+        "x" : -75.74753302428235,
+        "y" : -67.92171823864555
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "227",
+        "name" : "AI4-89",
+        "SUID" : 227,
+        "href" : "https://playingabout.atlassian.net/browse/AI4-89",
+        "IssueType_A" : "Story",
+        "selected" : false
+      },
+      "position" : {
+        "x" : -11.747533024282347,
+        "y" : -0.9217182386455534
+      },
+      "selected" : false
+    } ],
+    "edges" : [ {
+      "data" : {
+        "id" : "243",
+        "source" : "242",
+        "target" : "236",
+        "shared_name" : "AI4-100 (Blocks) AI4-97",
+        "name" : "AI4-100 (Blocks) AI4-97",
+        "interaction" : "Blocks",
+        "SUID" : 243,
+        "shared_interaction" : "Blocks",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "241",
+        "source" : "240",
+        "target" : "236",
+        "shared_name" : "AI4-99 (Blocks) AI4-97",
+        "name" : "AI4-99 (Blocks) AI4-97",
+        "interaction" : "Blocks",
+        "SUID" : 241,
+        "shared_interaction" : "Blocks",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "239",
+        "source" : "238",
+        "target" : "236",
+        "shared_name" : "AI4-98 (Blocks) AI4-97",
+        "name" : "AI4-98 (Blocks) AI4-97",
+        "interaction" : "Blocks",
+        "SUID" : 239,
+        "shared_interaction" : "Blocks",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "237",
+        "source" : "236",
+        "target" : "228",
+        "shared_name" : "AI4-97 (Blocks) AI4-88",
+        "name" : "AI4-97 (Blocks) AI4-88",
+        "interaction" : "Blocks",
+        "SUID" : 237,
+        "shared_interaction" : "Blocks",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "235",
+        "source" : "234",
+        "target" : "228",
+        "shared_name" : "AI4-92 (Blocks) AI4-88",
+        "name" : "AI4-92 (Blocks) AI4-88",
+        "interaction" : "Blocks",
+        "SUID" : 235,
+        "shared_interaction" : "Blocks",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "233",
+        "source" : "232",
+        "target" : "228",
+        "shared_name" : "AI4-91 (Blocks) AI4-88",
+        "name" : "AI4-91 (Blocks) AI4-88",
+        "interaction" : "Blocks",
+        "SUID" : 233,
+        "shared_interaction" : "Blocks",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "231",
+        "source" : "230",
+        "target" : "228",
+        "shared_name" : "AI4-90 (Blocks) AI4-88",
+        "name" : "AI4-90 (Blocks) AI4-88",
+        "interaction" : "Blocks",
+        "SUID" : 231,
+        "shared_interaction" : "Blocks",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "245",
+        "source" : "228",
+        "target" : "244",
+        "shared_name" : "AI4-88 (Blocks) AI4-105",
+        "name" : "AI4-88 (Blocks) AI4-105",
+        "interaction" : "Blocks",
+        "SUID" : 245,
+        "shared_interaction" : "Blocks",
+        "selected" : false
+      },
+      "selected" : false
+    }, {
+      "data" : {
+        "id" : "229",
+        "source" : "227",
+        "target" : "228",
+        "shared_name" : "AI4-89 (Blocks) AI4-88",
+        "name" : "AI4-89 (Blocks) AI4-88",
+        "interaction" : "Blocks",
+        "SUID" : 229,
+        "shared_interaction" : "Blocks",
+        "selected" : false
+      },
+      "selected" : false
+    } ]
+  }
+}}
---
+++ CapstoneJiraCloud/views/Graph/scripts/main.js
@@ -0,0 +1,34 @@
+$(function() {
+    "use strict";
+
+    function e(e, n) {
+        for (var t = n.length, o = 0; t >= o; o++) {
+            var s = n[o].title;
+            if (s === e) return n[o]
+        }
+        return null
+    }
+    var n = "#cy",
+        network = networks[Object.keys(networks)[0]],
+        style = styles[0];
+    $(n).cytoscape({
+        layout: {
+            name: "preset",
+            padding: 10, //space on edges when graph loads
+            avoidOverlap: true,
+            avoidOverlapPadding: 5,
+            //fit: true, // fits to viewport, in theory
+            //spacingFactor: 1.90
+        },
+        wheelSensitivity: 0.05,
+        autolock : true, //make nodes not moveable
+        //boxSelectionEnabled: !0,
+        ready: function() {
+            window.cy = this, 
+                    cy.load(network.elements), console.log(network);
+                    console.log(style);
+                    var o = e("default", style);
+                    null === o && (o = style), cy.style().fromJson(o.style).update()        
+        }
+    })
+});
